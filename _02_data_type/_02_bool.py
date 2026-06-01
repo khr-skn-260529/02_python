58b073f84a505e95892281e55d3a88cc0b720aea
kimerry-333 <khr030303@gmail.com>
2026-06-01 09:41:19 +0900

    20260602_bool(and,or)

--- _02_data_type/_02_bool.py
+++ _02_data_type/_02_bool.py
@@ -23,3 +23,45 @@ print("1 != 1:", 1 != 1) #False
 print(True)
 print(not True) #False
 print(not not True) #True
+
+#and 연산 (그리고)
+# A가 참이고 B도 참인 경우에 True
+# T and T == T
+# T and F == F
+# F and T == F
+# F and F == F
+
+print('--- and ---')
+print(100>0 and 1==1) #True
+print(30>20 and 123!=123)
+print(3<=-3 and 12>12)
+print(9>=9/9*9 and 12!=12+1)
+
+a=9>=9/9*9
+b=12!=12+1
+print(a and b)
+
+#or 연산
+#A또는B가 True이면 결과도 True이다
+#<-> A와 B가 False이면 결과는 False
+# T or T == T
+# T or F == T
+# F or T == T
+# F or F == F (중요!!)
+print('--- or ---')
+print(100>0 or  1==1)
+print(10*10==100 or 1!=1)
+print(100==0 or 10==10)
+print(10+20*5==100 or 30/10+5==7)
+
+# 합격(True) / 불합격(False) 여부
+#60점 이상일 시 합격
+print('--- 합격/불합격 ---')
+#input함수: 키보드 입력을 받는 함수 (str로 지정)
+#int함수 : str->int로 변환
+score = int(input('점수를 입력하세요'))
+print(score, type(score))
+result= score >= 60
+#print('합격여부 : ', result)
+### if문
+print('합격여부 : ', '합격' if result == True else '불합격')
