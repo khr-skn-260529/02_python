fe1f383441c697051a8dd27efdeae32d45d550fa
kimerry-333 <khr030303@gmail.com>
2026-06-09 09:02:01 +0900

    20260608_modul(namespace,pip,...)

---
+++ _07_module/skn/my_math.py
@@ -0,0 +1,23 @@
+#사용자 모듈
+
+if __name__ == '__main__':
+    print('my_math.py를 직접 실행하셨군요...')
+    pass
+else:
+    print('다른 py 파일에 my_math.py가 import 되었습니다.')
+    print('*'*100)
+    print('*'*100)
+
+
+pi:float=3.14
+
+x:int=20
+
+def get_circle_area(r:float)->float:
+    return pi*r*r
+
+#private 변수 설정
+# - private 설정이 강제되진 않음
+# - 대신 외부에서 직접 사용하는 것을 권장하지 않음
+# - import * 에서는 자동 제외됨
+__z:str='hello'
